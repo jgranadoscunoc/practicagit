1ce928af9cf3858e8405002b1c27f54ff081a1f7
Jose Moises Granados Guevara <32393687+jgranadoscunoc@users.noreply.github.com>
2021-08-05 17:47:55 -0600

    Revert "Agregue un ciclo al main y una linea al otro archivo"

--- Main.java
+++ Main.java
@@ -1,10 +1,9 @@
 public class Main {
 
 	public static void main(String[] args) {
-		for(int i = 0; i < 10; i++){
-			System.out.println("Hola Mundo !");                
-	                System.out.println("asdasda");
-		}
+		System.out.println("Hola Mundo !");
+                
+                System.out.println("asdasda");
 	}
 
 }
