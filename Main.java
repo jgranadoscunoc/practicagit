283df75e68a0f06b953d1efc8d19b9b612438894
Jose Granados <moi87g@gmail.com>
2021-08-05 18:13:40 -0600

    Task 5

--- Main.java
+++ Main.java
@@ -3,8 +3,12 @@ public class Main {
 	public static void main(String[] args) {
 		for(int i = 0; i < 10; i++){
 			System.out.println("Hola Mundo !");                
-	                System.out.println("asdasda");
+	                System.out.println("Hola Gente!");
 		}
+	}
+
+	public static int suma() {
+		return 5+6;
 	}
 
 }
