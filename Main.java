44e42c00d5528db3ccd988a177d50776aebf8bb5
Odra <fatima.tezo.99@gmail.com>
2021-08-05 18:38:27 -0600

    Otros cambios

--- Main.java
+++ Main.java
@@ -3,8 +3,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		for(int i = 0; i < 10; i++){
-			System.out.println("Hola Mundo !");                
-	                System.out.println("asdasda");
+			System.out.println("Hola Mo !");                
+	                System.out.println("---------");
 		}
 	}
 
